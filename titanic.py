33fc692311518d68e6da0d3aa828f301492bf629
ruchiagrawal <33382405+ruchi004@users.noreply.github.com>
2020-07-21 16:52:57 +0530

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -1,11 +1,14 @@
 import pandas as pd
 from numpy import array
+import openpyxl
+from openpyxl import Workbook
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix , accuracy_score
 
 dataset = pd.read_csv("C:\\Users\\RUCHI\\Desktop\\train.csv")
+test_data = pd.read_csv("C:\\Users\\RUCHI\\Desktop\\test.csv")
 
 features = ['Pclass' , 'Age' , 'Sex']
 X = dataset[features]
@@ -19,20 +22,39 @@ transform_sex = array(X['Sex'])
 fit_sex = label_encoder.fit_transform(transform_sex)
 X['Sex'] = fit_sex
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+X_test = test_data[features]
+
+valus = X_test['Age'].mean()
+X_test['Age'] = X_test['Age'].fillna(valus)
+
+label_encoder = LabelEncoder()
+transform_sex = array(X_test['Sex'])
+fit_sex = label_encoder.fit_transform(transform_sex)
+X_test['Sex'] = fit_sex
 
 classifier = DecisionTreeClassifier()
-classifier.fit(X_train, y_train)
+classifier.fit(X , y)
 
 y_pred = classifier.predict(X_test)
 
-C_matrix = confusion_matrix(y_test, y_pred)
-report = classification_report(y_test, y_pred)
-Accuracy = accuracy_score(y_test, y_pred)
+p_id = test_data['PassengerId']
 
-print(C_matrix)
-print(report)
-print(Accuracy)
+wb = Workbook()
+sheet =  wb.active
+sheet.title = "Prediction"
+
+c1 = sheet.cell(row = 1, column = 1)
+c1.value = 'PassengerId'
+c2 = sheet.cell(row = 1, column = 2)
+c2.value = 'Survived'
+
+for ii in range(2 , len(y_pred)+2):
+	c3 = sheet.cell(row = ii , column = 1)
+	c3.value = p_id[ii-2]
+	c4 = sheet.cell(row = ii , column = 2)
+	c4.value = y_pred[ii-2]
+
+wb.save("C:\\Users\\Ruchi\\Desktop\\titan.csv")
 
 # Precision = tp/tp+fp (indicates low # of fp)
 # Recall = tp/tp+fn (indicates low # of fn)
